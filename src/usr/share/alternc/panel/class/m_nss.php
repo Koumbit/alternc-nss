4327d4dd36f60ce5af832a13d8300ed9f4d9052a
ml-at-koumbit <info@koumbit.org>
2022-12-27 15:22:01 -0500

    added the management for a 'file_owner_prefix' which must be in the variable table of the database, which ensure alternc users have a prefix before their login in the ownership of the files.

--- src/usr/share/alternc/panel/class/m_nss.php
+++ src/usr/share/alternc/panel/class/m_nss.php
@@ -8,6 +8,7 @@ class m_nss
     protected $group_file;
     protected $passwd_file;
     protected $shadow_file;
+    protected $prefix;
     /** Hook function called when a user is created
      * This function add acccount to nss file
      * globals $cuid is the appropriate user
@@ -41,6 +42,7 @@ class m_nss
 
     public function define_files()
     {
+        $this->prefix = variable_get('file_owner_prefix');
         $this->define_group_file();
         $this->define_passwd_file();
         $this->define_shadow_file();
@@ -53,7 +55,8 @@ class m_nss
         $lines=array();
         $lines[]='##ALTERNC ACCOUNTS START##';
         while ($db->next_record()) {
-            $lines[] = $db->f('login').":x:".$db->f('uid').":";
+            $prefixed_login = $this->prefix . $db->f('login');
+            $lines[] = $prefixed_login.":x:".$db->f('uid').":";
         }
         $lines[]='##ALTERNC ACCOUNTS END##';
 
@@ -67,7 +70,8 @@ class m_nss
         $lines=array();
         $lines[]='##ALTERNC ACCOUNTS START##';
         while ($db->next_record()) {
-            $lines[] = $db->f('login').":x:".$db->f('uid').":".$db->f('uid')."::".getuserpath($db->f('login')).":/bin/false";
+            $prefixed_login = $this->prefix . $db->f('login');
+            $lines[] = $prefixed_login.":x:".$db->f('uid').":".$db->f('uid')."::".getuserpath($db->f('login')).":/bin/false";
         }
         $lines[]='##ALTERNC ACCOUNTS END##';
 
@@ -81,17 +85,18 @@ class m_nss
         $lines=array();
         $lines[]='##ALTERNC ACCOUNTS START##';
         while ($db->next_record()) {
-	    // shadow fields (9) :
-	    // 1. login
-	    // 2. encrypted password or * to prevent login
-	    // 3. date of last password change or '' meaning that password aging features are disabled
-	    // 4. minimum password age or '' or 0 meaning no minimum age
-	    // 5. maximum password age or '' meaning no maximum password age, no password warning period, and no password inactivity period
-	    // 6. password warning period or '' or 0 meaning there are no password warning period
-	    // 7. password inactivity period or '' for no enforcement
-	    // 8. account expiration date or '' for no expiration
-	    // 9. reserved
-	    $fields = array($db->f('login'), '*', '', '', '', '', '', '', '');
+            // shadow fields (9) :
+            // 1. login
+            // 2. encrypted password or * to prevent login
+            // 3. date of last password change or '' meaning that password aging features are disabled
+            // 4. minimum password age or '' or 0 meaning no minimum age
+            // 5. maximum password age or '' meaning no maximum password age, no password warning period, and no password inactivity period
+            // 6. password warning period or '' or 0 meaning there are no password warning period
+            // 7. password inactivity period or '' for no enforcement
+            // 8. account expiration date or '' for no expiration
+            // 9. reserved
+            $prefixed_login = $this->prefix . $db->f('login');
+            $fields = array($prefixed_login, '*', '', '', '', '', '', '', '');
             $lines[] = implode(':', $fields);
         }
         $lines[]='##ALTERNC ACCOUNTS END##';
